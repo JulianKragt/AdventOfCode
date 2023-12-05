c2a23ce9b4b031e9b1328fb1c2545974c2d84b57
jkragt <jkragt@outlook.com>
2023-12-05 22:26:21 +0100

    begin day 1 puzzle 1

--- 2023/day_1_1/src/main.rs
+++ 2023/day_1_1/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./input.txt").expect("couldn't read file");
+    let lines = contents.lines();
+    let _numbers = lines.map(|line| {
+        let numbers: Vec<char> = line.chars().filter(|char| {
+            char.is_numeric()
+        }).collect();
+
+        String::new().push(numbers.first())).push(numbers.last())
+    });
+
+    println!(
+        "{:?}",
+        _numbers.clone().next()
+    );
+
 }
