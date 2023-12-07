10cee51ffc858b89948ffd1ab8fbd3006a50caf7
jkragt <jkragt@outlook.com>
2023-12-07 18:56:59 +0100

    remove target and .idea

--- 2023/day_1_2/target/debug/deps/day_1_2-7396f6c12b1587a5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jkrag\Projects\AdventOfCode\2023\day_1_2\target\debug\deps\day_1_2-7396f6c12b1587a5.exe: src\main.rs
-
-C:\Users\jkrag\Projects\AdventOfCode\2023\day_1_2\target\debug\deps\day_1_2-7396f6c12b1587a5.d: src\main.rs
-
-src\main.rs:
