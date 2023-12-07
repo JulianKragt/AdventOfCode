1890d46ad2702b86b274f0594dbd136749ecc574
jkragt <jkragt@outlook.com>
2023-12-07 19:05:48 +0100

    add replace but not working

--- 2023/day_1_2/src/main.rs
+++ 2023/day_1_2/src/main.rs
@@ -4,6 +4,19 @@ fn main() {
     let contents = fs::read_to_string("./input.txt").expect("couldn't read file");
     let lines = contents.lines();
     let _numbers = lines.map(|line| {
+        let line = line
+            .replace("zero", "0")
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+
+
         let numbers: Vec<char> = line.chars().filter(|char| {
             char.is_numeric()
         }).collect();
